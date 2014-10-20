bc9359dfcae89c848c8342eac41e7e4d93a899ed
Matheus de Souza <mathew_souza@hotmail.com>
2014-10-20 16:51:34 -0200

    aula30 - Detecção de medidas e modo paisagem/retrato - #1

---
+++ layout/js/medidas.js
@@ -0,0 +1,18 @@
+var spanLargura= document.getElementById('largura');
+var spanAltura = document.getElementById('altura');
+
+window.onresize = function() {
+    altura = window.innerHeight;
+    largura = window.innerWidth;
+    
+    spanLargura.innerHTML = largura + 'px ';
+    spanAltura.innerHTML = altura + 'px ';
+};
+window.onload = function() {
+    altura = window.innerHeight;
+    largura = window.innerWidth;
+    
+    spanLargura.innerHTML = largura + 'px ';
+    spanAltura.innerHTML = altura + 'px ';
+};
+
