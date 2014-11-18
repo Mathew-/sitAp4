82c25a50e31258d7176f24c98ed1e9b823f8d51c
Mathew- <mathew_souza@hotmail.com>
2014-11-18 15:55:47 -0200

    Adicionado arquivos do login

--- application/models/usuarios_model.php
+++ application/models/usuarios_model.php
@@ -17,9 +17,9 @@ class Usuarios_model extends CI_Model {
     function listar() {
         //Listagem da tabela usuário
         $query = $this->db
-                ->order_by('nome','asc') //Ordem alfabética
+                ->order_by('nome', 'asc') //Ordem alfabética
                 ->get('usuario');
-        
+
         return $query->result();
     }
 
@@ -38,6 +38,22 @@ class Usuarios_model extends CI_Model {
     function deletar($idusuario) {
         $this->db->where('idusuario', $idusuario);
         return $this->db->delete('usuario');
+    }
+
+    function login($username, $password) {
+        $this->db->select('idusuario, email, senha');
+        $this->db->from('usuario');
+        $this->db->where('email', $username);
+        $this->db->where('senha', $password);
+        $this->db->limit(1);
+
+        $query = $this->db->get();
+
+        if ($query->num_rows() == 1) {
+            return $query->result();
+        } else {
+            return false;
+        }
     }
 
 }
